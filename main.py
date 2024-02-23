2ae13accba41eb379a61febf15749f207e2943e3
Gabriel Moura <55935381+Ga-Moura@users.noreply.github.com>
2024-02-23 13:45:44 +0000

    2x

--- main.py
+++ main.py
@@ -409,11 +409,11 @@ def carregamento():
     with st.container():
 
         st.subheader("Indicadores")
-        lcarr,_,_ ,_,_ ,_,_ = st.columns(7)
+        _,lcarr,_ ,_,_ ,_,_ = st.columns(7)
 
         lcarr.image(Image.open(cadastro).resize((100,100)))
 
-        lcarr.markdown(f'&nbsp;&nbsp;&nbsp;&nbsp;[Aderência - Crregamento]({pbi_carregamento_entrega})')
+        st.markdown(f'&nbsp;&nbsp;&nbsp;&nbsp;[Aderência - Crregamento]({pbi_carregamento_entrega})')
 
 
 
