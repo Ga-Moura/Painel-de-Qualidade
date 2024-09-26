1726bfb1b7fc99e770d2a19e48d79eb8807e051d
Gabriel Moura <55935381+Ga-Moura@users.noreply.github.com>
2024-09-26 17:47:10 +0000

    attl

--- main.py
+++ main.py
@@ -655,8 +655,8 @@ def torre():
 
         st.subheader('Coletor de Qualidesk')
 
-        _,_,col1,_= st.columns(4)
-        _,_,text1,_ = st.columns(4)
+        _,col1,_,_= st.columns(4)
+        _,text1,_,_ = st.columns(4)
 
 
         col1.image(Image.open(qrcode_qualidesk).resize((200,200)))
