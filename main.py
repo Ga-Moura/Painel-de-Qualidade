48ce93bef8d57f62d2b04af2b3e1aa0e9432e8bf
Gabriel Moura <55935381+Ga-Moura@users.noreply.github.com>
2024-09-26 17:49:25 +0000

    remov ;

--- main.py
+++ main.py
@@ -661,7 +661,7 @@ def torre():
 
         col1.image(Image.open(qrcode_qualidesk).resize((100,100)))
         
-        text1.write(f'&nbsp;&nbsp;&nbsp;&nbsp;;Survey - Qualidesk')
+        text1.write(f'&nbsp;&nbsp;&nbsp;&nbsp;Qualidesk')
 
 
 #Botões e Páginas
